3a30a086466debabfc99df3b68119b430372d1a1
Weslley Martins Batista <2003weslleymb@gmail.com>
2026-04-28 16:10:40 -0300

    style: adição de componentes visuais

--- Modulo 2/UC9 Programar aplicativos computacionais com integração de banco de dados para desktop/Atividade 1/Comercial/Eclipse/ControleDeEstoqueDeProdutos/src/main/Main.java
+++ Modulo 2/UC9 Programar aplicativos computacionais com integração de banco de dados para desktop/Atividade 1/Comercial/Eclipse/ControleDeEstoqueDeProdutos/src/main/Main.java
@@ -1,10 +1,20 @@
 package main;
 
+import javax.swing.JFrame;
+
+import view.LoginView;
+
 public class Main {
 
 	public static void main(String[] args) {
+		LoginView telaLogin = new LoginView();
+		JFrame janela = new JFrame("Gestão de estoque");
 		
-
+		
+		janela.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		
+		janela.add(telaLogin.setTelaLogin());
+		janela.setVisible(true);
 	}
 
 }
--- Modulo 2/UC9 Programar aplicativos computacionais com integração de banco de dados para desktop/Atividade 1/Comercial/Eclipse/ControleDeEstoqueDeProdutos/src/view/LoginView.java
+++ Modulo 2/UC9 Programar aplicativos computacionais com integração de banco de dados para desktop/Atividade 1/Comercial/Eclipse/ControleDeEstoqueDeProdutos/src/view/LoginView.java
@@ -1,25 +1,131 @@
 package view;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Image;
+import java.awt.Insets;
+
 import javax.swing.*;
+import javax.swing.border.TitledBorder;
 
 public class LoginView {
-	//JPanel telaLogin = new JPanel();
-		JPanel cardLogin = new JPanel();
-			JTextField textNome = new JTextField();
-			JTextField textSenha = new JTextField();
-			JButton botaoEntrar = new JButton(); 
-			
+	JPanel telaLogin = new JPanel();
+		ImageIcon logo = new ImageIcon("resources/images/logoEstozem.png"); JLabel labelLogo;
+		
+		JPanel cardLogin = new JPanel(); TitledBorder borderLogin; Font borderFonte;
+		
+			JPanel abaUsuario = new JPanel();
+				JLabel labelUsuario = new JLabel("Usuario");
+				JTextField textusuario = new JTextField();
+				
+			JPanel abaSenha = new JPanel();
+				JLabel labelSenha = new JLabel("Senha");
+				JTextField textSenha = new JTextField();
+				
+			JPanel abaEntrar = new JPanel();
+				JButton botaoEntrar = new JButton("Entrar"); 
+		
 	public LoginView() {
+		
+	}
+	private void gerenciarComponentes() {
+		// - ABA USUARIO
+		labelUsuario.setFont(new Font("Arial", Font.BOLD, 18));
+		labelUsuario.setBorder(BorderFactory.createEmptyBorder(20, 50, 0, 0)); // top, left, bottom, right
+		
+		textusuario.setPreferredSize(new Dimension(600, 30)); 
+		textusuario.setMaximumSize(new Dimension(600, 30));
+		
+		// - ABA SENHA
+		labelSenha.setFont(new Font("Arial", Font.BOLD, 18));
+		labelUsuario.setBorder(BorderFactory.createEmptyBorder(20, 50, 0, 0)); // top, left, bottom, right
+		
+		textSenha.setPreferredSize(new Dimension(600, 30)); 
+		textSenha.setMaximumSize(new Dimension(600, 30));
+		
+		// - ABA ENTRAR
+		botaoEntrar.setPreferredSize(new Dimension(100, 30));
+		botaoEntrar.setMaximumSize(new Dimension(100, 30));
+		//botaoEntrar.setAlignmentX(Component.RIGHT_ALIGNMENT);
+		
+		// - ADIÇÃO EM PANEIS
+		abaUsuario.setLayout(new BoxLayout(abaUsuario, BoxLayout.Y_AXIS));
+		abaUsuario.add(labelUsuario);
+		abaUsuario.add(textusuario);
+		
+		abaSenha.setLayout(new BoxLayout(abaSenha, BoxLayout.Y_AXIS));
+		abaSenha.add(labelSenha);
+		abaSenha.add(textSenha);
+		
+		abaEntrar.setLayout(new BoxLayout(abaEntrar, BoxLayout.Y_AXIS));
+		abaEntrar.add(botaoEntrar);
+		
 		
 	}
 	
 	private void gerenciarLogin() {
+		gerenciarComponentes();
 		
+		// LOGO
+		//labelLogo = new JLabel(logo);
+		Image img = logo.getImage();
 		
+		int largura = 100;
+		int altura = 100;
+		
+		Image imgRedimensionada = img.getScaledInstance(largura, altura, Image.SCALE_SMOOTH);
+		
+		ImageIcon iconRedimensionado = new ImageIcon(imgRedimensionada);
+		
+		labelLogo = new JLabel(iconRedimensionado);
+		
+		// CARD LOGIN
+		cardLogin.setPreferredSize(new Dimension(500, 500));
+		cardLogin.setMaximumSize(new Dimension(500, 500));
+		
+		borderFonte = new Font("Arial", Font.BOLD, 16);
+		
+		borderLogin = BorderFactory.createTitledBorder(
+			    BorderFactory.createEtchedBorder(), 
+			    "Login", 
+			    TitledBorder.LEFT, 
+			    TitledBorder.TOP, 
+			    borderFonte,
+			    Color.BLUE
+			);
+		
+		cardLogin.setBorder(borderLogin);
+		//cardLogin.setBackground(Color.GRAY);
+		
+		cardLogin.setLayout(new BoxLayout(cardLogin, BoxLayout.Y_AXIS));
+		
+		cardLogin.add(abaUsuario);
+		cardLogin.add(abaSenha);
+		cardLogin.add(abaEntrar);
+		
+		// TELA
+		telaLogin.setLayout(new GridBagLayout());
+		GridBagConstraints gbc = new GridBagConstraints();
+		
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		
+		telaLogin.add(labelLogo, gbc);
+		
+		gbc.gridx = 0;
+		gbc.gridy = 1;
+		
+		telaLogin.add(cardLogin, gbc);
 	}
 	
 	public JPanel setTelaLogin() {
 		gerenciarLogin();
-		return cardLogin;
+		//return cardLogin;
+		return telaLogin;
 	}
 }
